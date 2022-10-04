b9dcd81ff295a186fd9b84205ab5c6284e95c5c8
iamawesomearyan <iamawesomearyan@gmail.com>
2022-10-03 21:07:50 -0400

    everything

---
+++ Aryanarray.java
@@ -0,0 +1,9 @@
+public class Aryanarray {
+    public int findMaxConsecutiveOnes(int[] nums) {
+       // int oldLength = 0;
+        //int newLength = 0;
+        //for()
+        return 1;
+    }
+    
+}
--- Cool.java
+++ Cool.java
@@ -1,1 +1,6 @@
-public class Cool{}
+public class Cool{
+    public static void main(String args[]){
+        Aryanarray x = new Aryanarray();
+        System.out.println(x.findMaxConsecutiveOnes(null));
+    }
+}
